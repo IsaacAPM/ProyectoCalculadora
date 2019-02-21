1821d81a1fa87f0f223063e46ed74df6c108a278
unknown <isaacapimentelm@gmail.com>
2019-02-20 22:46:20 -0600

    Version final del control de erroes, incluye javadoc

--- ProyectoCalculadora/src/proyectocalculadora/Calculadora.java
+++ ProyectoCalculadora/src/proyectocalculadora/Calculadora.java
@@ -7,7 +7,17 @@ import Pila.PilaA;
  */
 public class Calculadora {
     
-    
+    /**<pre>
+     * Revisa la sintaxis de una expresión matemática
+     * que únicamente usa las 4 operaciones aritméticas
+     * básicas.
+     * </pre>
+     * @param prob Expresión matemática a revisar
+     * @return <ul>
+     *      <li>true: si la sintaxis de la expresión es correcta</li>
+     *      <li>false: si la sintaxis de la expresión es incorrecta</li>
+     * </ul>
+     */
     public static boolean detectorErroresSintaxis(String prob){
         boolean resp = true;
         boolean band = true;
@@ -17,35 +27,45 @@ public class Calculadora {
         int j;
         int f = prob.length() - 1;
         
-        if(prob.charAt(f) == '+' || prob.charAt(f) == '-' || prob.charAt(f) == '*' || prob.charAt(f) == '/'){
-            resp = false;
+        //verifico que el ultimo caracter de la expresión no sea un operador o un parentesis de cierre
+        if(prob.charAt(f) == '+' || prob.charAt(f) == '-' || prob.charAt(f) == '*' || prob.charAt(f) == '/' || prob.charAt(f) == '('){
+            resp = false;// en caso de serlo, la respuesta es false
         }
         
+        //comienso a revisar la expresión desde el inico buscando operadores + y -, ya que estos no afectan al inico
+        //dejo de revisar di encuntro un número o un operador diferente a + y - 
         while(i < prob.length() && resp && band){
-            if(prob.charAt(i) == '*' || prob.charAt(i) == '/'){
-                resp = false;
-            } else if(prob.charAt(i) == '.'){
+            //verifico si el termino es un *,/ o )
+            if(prob.charAt(i) == '*' || prob.charAt(i) == '/' || prob.charAt(i) == ')'){
+                resp = false;// en caso de serlo la respuesta es false
+            } else if(prob.charAt(i) == '.'){//si no fue lo anterior verifico si es un punto
+                //si si es un punto reviso si el siguiente es otro punto o un operador
                 if(prob.charAt(i+1) == '.' || prob.charAt(i) == '*' || prob.charAt(i) == '/' || prob.charAt(i) == '+' || prob.charAt(i) == '-'){
-                    resp = false;
-                } else {
-                    band = false;
+                    resp = false;//en caso de serlo la respuesta es false
+                } else {//si no fue nada de lo anterior entonses debe ser un número o un parentesis
+                    band = false;//salgo del while
                 }
-            } else if( prob.charAt(i) != '+' && prob.charAt(i) != '-'){
-                band = false;
+            } else if( prob.charAt(i) != '+' && prob.charAt(i) != '-'){//si no fue nada de lo anterior reviso que no sea ni un + ni un -
+                band = false;//si no es etonses salgo del while
             } else {
-                i++;
+                i++;// si si es un + o un -, aumeto 1 a la i para revisar el siguiente
             }
         }
         
+        //si al final se salio del while por que recorrio toda la cadena entonses quiere decir
+        //que la expresión solo tiene operadores + y -
         if(i > prob.length()){
-            resp = false;
+            resp = false;// si es el caso la respuesta es false
         }
         
+        //si no fue el caso anterior entonses quiere decir que encontro un número o un parentesis
+        //prosedo a revisar que los parentesis esten bien balanseados y que no haya otros errores de
+        //sintaxis
         while(i<prob.length() && resp){
                     
             pa=prob.charAt(i);
             
-            if(pa=='('||pa=='['||pa=='{'){
+            if(pa=='('){
                 p.push(pa);
             }
             else{
@@ -58,40 +78,30 @@ public class Calculadora {
                             resp=false;
                         }
                         break;
-                    case ']':
-                        if(!p.isEmpty()&&p.peek()=='['){
-                            p.pop();
-                        }
-                        else{
-                            resp=false;
-                        }
-                        break;
-                    case '}':
-                        if(!p.isEmpty()&&p.peek()=='{'){
-                            p.pop();
-                        }
-                        else{
-                            resp=false;
-                        }
-                        break;
                     case '.':
+                        //en caso de que el caracter altual sea un punto reviso si antes de el hay un operador
                         if(prob.charAt(i-1) == '+' || prob.charAt(i-1) == '-' || prob.charAt(i-1) == '*' || prob.charAt(i-1) == '/'){
+                            //si si hay resviso hay un caracter despues
                             if((i+1) < prob.length()){
-                                if(prob.charAt(i+1) == '.' || prob.charAt(i+1) == '+' || prob.charAt(i+1) == '-' || prob.charAt(i+1) == '*' ||prob.charAt(i+1) == '/' || prob.charAt(i+1) == '(' || prob.charAt(i+1) == ')' || prob.charAt(i+1) == '[' || prob.charAt(i+1) == ']' || prob.charAt(i+1) == '{' || prob.charAt(i+1) == '}'){
-                                    resp = false;
+                                //si si hay reviso que es ese caracter
+                                if(prob.charAt(i+1) == '.' || prob.charAt(i+1) == '+' || prob.charAt(i+1) == '-' || prob.charAt(i+1) == '*' ||prob.charAt(i+1) == '/' || prob.charAt(i+1) == '(' || prob.charAt(i+1) == ')'){
+                                    resp = false;//si no es un numero la respuesta es false
                                 }
-                            }else {
+                            } else {//si no hay otro caracter despues la respuesta es false
                                 resp = false;
                             }
                         }
+                        //si lo que esta antes no es un operador y si hay otro caracter despues
                         if((i+1) < prob.length()){
+                            //reviso que hay en los caracteres siguietes, si es alguno de los siguientes casos la respuesta sera false
+                            //5.2.2, 5..2, etc. 
                             if(prob.charAt(i+1) == '.'){
                             resp = false;
                             } else {
                                 j = i+1;
                                 band = true;
                                 while(j < prob.length() && band){
-                                    if(prob.charAt(j) == '+' || prob.charAt(j) == '-' || prob.charAt(j) == '*' || prob.charAt(j) == '/' || prob.charAt(j) == '(' || prob.charAt(j) == ')' || prob.charAt(j) == '{' || prob.charAt(j) == '}' || prob.charAt(j) == '[' || prob.charAt(j) == ']'){
+                                    if(prob.charAt(j) == '+' || prob.charAt(j) == '-' || prob.charAt(j) == '*' || prob.charAt(j) == '/' || prob.charAt(j) == '(' || prob.charAt(j) == ')'){
                                         band = false;
                                     } else if (prob.charAt(j) == '.'){
                                         resp = false;
@@ -99,10 +109,13 @@ public class Calculadora {
                                     }
                                     j++;
                                 }
-                                i = j-1;
+                                i = j-2;
                             } 
                         }                  
                         break;
+                    //reviso los casos en los que el caracter actual es un operador
+                    //solo sera false la respuesta si el siguiente caracter es otro
+                    //operador
                     case '+':
                         if(prob.charAt(i+1) == '*' || prob.charAt(i+1) == '/'){
                             resp = false;
@@ -114,12 +127,12 @@ public class Calculadora {
                         }
                         break;
                     case '*':
-                        if(prob.charAt(i+1) == '+' || prob.charAt(i+1) == '-' || prob.charAt(i+1) == '*' || prob.charAt(i+1) == '/'){
+                        if(prob.charAt(i+1) == '+' || prob.charAt(i+1) == '*' || prob.charAt(i+1) == '/'){
                             resp = false;
                         }
                         break;
                     case '/':
-                        if(prob.charAt(i+1) == '+' || prob.charAt(i+1) == '-' || prob.charAt(i+1) == '*' || prob.charAt(i+1) == '/'){
+                        if(prob.charAt(i+1) == '+' || prob.charAt(i+1) == '*' || prob.charAt(i+1) == '/'){
                             resp = false;
                         }
                         break;
@@ -133,29 +146,43 @@ public class Calculadora {
             resp=false;
         }
         
-        return resp;
+        return resp;//regreso la respuesta final
     }
     
+    /**<pre>
+     * Revisa las operaciones de una expresión matemática
+     * que únicamente usa las 4 operaciones aritméticas
+     * básicas; busca que no se comentan erros matemáticos
+     * como unicamente se usan las 4 operaciones basicas el 
+     * único error posible es dividir entre 0.
+     * </pre>
+     * @param prob Expresión matemática a revisar
+     * @return <ul>
+     *      <li>true: si nunca se cometio un error matemático(dividir entre 0)</li>
+     *      <li>false: si se cometio un error matemático(dividir entre 0)</li>
+     * </ul>
+     */
     public static boolean detectorErroresMath(String prob){
         boolean resp = true;
         boolean band;
         int i = 0;
         int j;
         
+        //busco en la cadena un operador de división
         while(i < prob.length() && resp){
-            if(prob.charAt(i) == '/' && (i+1) < prob.length()){
-                if(prob.charAt(i+1) == '0'){
+            if(prob.charAt(i) == '/' && (i+1) < prob.length()){// caundo lo ecuentro me aseguro que exista un caracter despues
+                if(prob.charAt(i+1) == '0'){//si el caracter que sigue es 0 reviso que caso es
                     if((i+2) < prob.length()){
                         j = i+2;
                         band = true;
                         while(j < prob.length() && band){
-                            if(prob.charAt(j) == '+' || prob.charAt(j) == '-' || prob.charAt(j) == '*' || prob.charAt(j) == '/' || prob.charAt(j) == '(' || prob.charAt(j) == ')' || prob.charAt(j) == '{' || prob.charAt(j) == '}' || prob.charAt(j) == '[' || prob.charAt(j) == ']'){
-                                resp = false;
-                                band = false;
+                            if(prob.charAt(j) == '+' || prob.charAt(j) == '-' || prob.charAt(j) == '*' || prob.charAt(j) == '/' || prob.charAt(j) == '(' || prob.charAt(j) == ')'){
+                                resp = false; //este if revisa el caso x/000+y, la cantidad de 0 en este caso debe ser mayor igual a 1 
+                                band = false; //este caso indica que hay un error
                             } else if(prob.charAt(j) == '.'){
-                                band = false;
+                                band = false; //este if revisa el caso x/0.y, este caso no es un error 
                             } else if(prob.charAt(j) != '0'){
-                                band = false;
+                                band = false; //este if revisa el caso x/000y, este caso tampoco debe marcar error
                             } else {
                                 j++;
                             }
@@ -173,7 +200,7 @@ public class Calculadora {
             i++;
         }
         
-        return resp;
+        return resp;// regresa la respuesta final
     }
     
     public static String convertirPostfija(String cadena){
--- ProyectoCalculadora/src/proyectocalculadora/ProyectoCalculadora.java
+++ ProyectoCalculadora/src/proyectocalculadora/ProyectoCalculadora.java
@@ -15,14 +15,14 @@ public class ProyectoCalculadora {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*System.out.println(Calculadora.detectorErroresSintaxis("5+2"));
+        System.out.println(Calculadora.detectorErroresSintaxis("5+2"));
         System.out.println(Calculadora.detectorErroresSintaxis("(5+2)*6"));
         System.out.println(Calculadora.detectorErroresSintaxis("(5+6)(56+1"));
-        System.out.println(Calculadora.detectorErroresSintaxis("{25*.89+(52*85)}-[25/82]"));
+        System.out.println(Calculadora.detectorErroresSintaxis("(25*.89+(52*85))-(25/82)"));
         System.out.println(Calculadora.detectorErroresSintaxis("++++5-25.23.2"));
         System.out.println(Calculadora.detectorErroresSintaxis("++++.5+"));
         
-        System.out.println(Calculadora.detectorErroresMath("5+5/0"));
+        /*System.out.println(Calculadora.detectorErroresMath("5+5/0"));
         System.out.println(Calculadora.detectorErroresMath("5+5/0.5"));
         System.out.println(Calculadora.detectorErroresMath("5+5/05"));
         System.out.println(Calculadora.detectorErroresMath("5+5/0+6"));
@@ -31,15 +31,17 @@ public class ProyectoCalculadora {
         System.out.println(Calculadora.detectorErroresMath("5+5/000000"));
         System.out.println(Calculadora.detectorErroresMath("5+5/0000001"));
         System.out.println(Calculadora.detectorErroresMath("5+5/00000020"));
-        System.out.println(Calculadora.detectorErroresMath("5+5/001+5"));
+        System.out.println(Calculadora.detectorErroresMath("5+5/001+5"));*/
         
         String cadena="50.2+2.5/(3+45)*3-6/2*(1+1)-1";
-        
-        System.out.println(Calculadora.convertirPostfija(cadena));
+        System.out.println(Calculadora.detectorErroresSintaxis(cadena));
+        /*System.out.println(Calculadora.convertirPostfija(cadena));
         System.out.println(Calculadora.convertirPostfija("(25+45)/85"));*/
         
         System.out.println(Calculadora.detectorErroresSintaxis("5+5/5+.5*(5+5)"));
-
+        System.out.println(Calculadora.detectorErroresSintaxis("6/-3"));
+        System.out.println(Calculadora.detectorErroresSintaxis("(2/1.0)"));
+        System.out.println(Calculadora.detectorErroresSintaxis("2/1."));
     }
     
 }
